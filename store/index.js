74c516905677b74b546c9a32c6ac971f25bb7dcf
Michael Roberts <51658879+robertsmikej@users.noreply.github.com>
2021-05-10 13:35:32 -0600

    Add files via upload

---
+++ store/index.js
@@ -0,0 +1,120 @@
+const axios = require('axios');
+
+export const state = () => ({
+    sitewide: {},
+    springs: {},
+    pagetype: {},
+    popular: {},
+    rating: {},
+    within2Hours: {},
+    articles: {},
+    recentArticles: {},
+    articlesTips: {},
+    tripReports: {},
+    numOfSprings: {}
+})
+
+export const mutations = {
+    setSitewide(state, data) {
+        state.sitewide = data;
+    },
+    setSprings(state, data) {
+        for (var spring in data.data) {
+            state.springs[data.data[spring].condensed_page_name] = data.data[spring];
+        }
+        state.numOfSprings.number = data.data.length;
+    },
+    setPageType(state, data) {
+        for (var page in data.data) {
+            state.pagetype[data.data[page].page_type] = data.data[page];
+        }
+    },
+    setPopular(state, data) {
+        for (var popular in data.data) {
+            state.popular[data.data[popular].condensed_page_name] = data.data[popular];
+        }
+    },
+    setRating(state, data) {
+        for (var rate in data.data) {
+            state.rating[data.data[rate].condensed_page_name] = data.data[rate];
+        }
+    },
+    setWithin2Hours(state, data) {
+        for (var within2 in data.data) {
+            state.within2Hours[data.data[within2].condensed_page_name] = data.data[within2];
+        }
+    },
+    setArticles(state, data) {
+        for (var article in data) {
+            state.articles[data[article].condensed_title] = data[article];
+        }
+//        state.articles = data;
+    },
+    setRecentArticles(state, data) {
+        for (var articleRecent in data) {
+            state.recentArticles[data[articleRecent].condensed_title] = data[articleRecent];
+        }
+//        state.articles = data;
+    },
+    setArticlesTips(state, data) {
+        for (var articlesTip in data) {
+            state.articlesTips[data[articlesTip].condensed_title] = data[articlesTip];
+        }
+//        state.articlesTips = data;
+    },
+    setArticlesTripReports(state, data) {
+        for (var tripReport in data) {
+            state.tripReports[data[tripReport].condensed_title] = data[tripReport];
+        }
+//        state.tripReports = data;
+    }
+}
+
+export const getters = {
+    sitewide: state => state.sitewide,
+    springs: state => state.springs,
+    pagetype: state => state.pagetype,
+    popular: state => state.popular,
+    rating: state => state.rating,
+    within2Hours: state => state.within2Hours,
+    articles: state => state.articles,
+    recentArticles: state => state.recentArticles,
+    articlesTips: state => state.articlesTips,
+    tripReports: state => state.tripReports,
+    numOfSprings: state => state.numOfSprings
+}
+
+export const actions = {
+    async nuxtServerInit ({commit}) {
+        const sitewide = await axios.get("https://www.hotspringing.com/directus/public/_/items/sitewide?fields=*.*").then((response) => {
+            commit("setSitewide", response.data.data[0]);
+        });
+        const springs = await axios.get("https://www.hotspringing.com/directus/public/_/items/hotsprings?fields=*.*.*&sort=name").then((response) => {
+            commit("setSprings", response.data);
+        });
+        const pageTypes = await axios.get("https://www.hotspringing.com/directus/public/_/items/pages?fields=*.*").then((response) => {
+            commit("setPageType", response.data);
+        });
+        const popu = await axios.get("https://www.hotspringing.com/directus/public/_/items/hotsprings?filter[popularity][gt]80&sort=-popularity&fields=*.*.*&limit=6").then((response) => {
+            commit("setPopular", response.data);
+        });
+        const rate = await axios.get("https://www.hotspringing.com/directus/public/_/items/hotsprings?filter[rating][gt]80&sort=-rating&fields=*.*.*&limit=6").then((response) => {
+            commit("setRating", response.data);
+        });
+        const within2hours = await axios.get("https://www.hotspringing.com/directus/public/_/items/hotsprings?filter[num_of_minutes_from_boise][lt]=121&fields=*.*.*&limit=6").then((response) => {
+            commit("setWithin2Hours", response.data);
+        });
+        const art = await axios.get("https://www.hotspringing.com/directus/public/_/items/articles?fields=*.*.*").then((response) => {
+            commit("setArticles", response.data.data);
+        });
+        const recentArticles = await axios.get("https://www.hotspringing.com/directus/public/_/items/articles?fields=*.*.*&limit=6").then((response) => {
+            commit("setRecentArticles", response.data.data);
+        });
+        const artTips = await axios.get("https://www.hotspringing.com/directus/public/_/items/articles?filter[article_type]=tips_and_tricks&fields=*.*.*").then((response) => {
+            commit("setArticlesTips", response.data.data);
+        });
+        const artTripReports = await axios.get("https://www.hotspringing.com/directus/public/_/items/articles?filter[article_type]=trip_report&fields=*.*.*").then((response) => {
+            commit("setArticlesTripReports", response.data.data);
+        });
+    }
+}
